6e22a04717bf9f037909d0316d8bb5e99ce86105
jpsong <jpsong@dst57240.cn1.global.ctrip.com>
2019-09-17 10:49:29 +0800

    切图demo

--- app/views/containers/hello-world/index.js
+++ app/views/containers/hello-world/index.js
@@ -1,24 +1,66 @@
-import React, { Component } from 'react'
-// import { Icon } from 'antd'
+import React, { useState, useRef, useEffect } from 'react'
+// import { Input } from 'antd'
 import './style.less'
 import './style.css'
 
-class HelloWorld extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-    }
+function Counter ({ initialCount }) {
+  const [count, setCount] = useState(initialCount)
+  const prevCountRef = useRef(initialCount)
+  console.log('counter init')
+  useEffect(() => {
+    prevCountRef.current = initialCount
+  })
+
+  console.log('initialCount', initialCount)
+  console.log('prevCountRef.current', prevCountRef.current)
+  console.log('count', count)
+  // initialCount !== prevCountRef.current 确保在参数变化时执行setCount
+  // 在参数变化时，setCount执行，再次执行到这里时，prevCountRef.current还没更新，initialCount !== prevCountRef.current 依旧成立。
+  // 所以需要 count !== initialCount，因为此时 count已经更新了，确保不会重复执行
+  if (initialCount !== prevCountRef.current && count !== initialCount) {
+    setCount(initialCount)
   }
-  render () {
-    return (
-      <React.Fragment>
-        <div className='title'>
-            Hello, express-react-dev-template
-        </div>
-        {/* <Icon type='android' /> */}
-      </React.Fragment>
-    )
-  }
+
+  return <div>
+    <p>counter2:{count}</p>
+    <div>
+      <button onClick={() => { setCount(count + 1) }}>+</button>
+    </div >
+  </div >
 }
 
-export default HelloWorld
+function App () {
+  const [count, setCount] = useState(0)
+  console.log('app init')
+  return (
+    <div className='App'>
+      <h3>hook ver</h3>
+      <p>counter1:{count}</p>
+      <div>
+        <button onClick={() => { setCount(count + 1) }}>+</button>
+      </div>
+      <Counter initialCount={count} />
+    </div>
+  )
+}
+
+// import ReactCrop from 'react-image-crop'
+// import 'react-image-crop/dist/ReactCrop.css'
+// const state = {
+//   unit: 'px', // default, can be 'px' or '%'
+//   // x: 130,
+//   // y: 50,
+//   width: 200,
+//   height: 200
+// }
+
+// function App () {
+//   const [crop, setCrop] = useState(state)
+//   return <ReactCrop src='/static/bg.jpg'
+//     crop={crop}
+//     disabled
+//     onChange={crop => setCrop(crop)}
+//   />
+// }
+
+export default App
