8657e0d608eb4924387207620c2f188467930333
jpsong <jpsong@dst57240.cn1.global.ctrip.com>
2019-10-08 15:50:39 +0800

    add count down demo

--- app/views/containers/trigger/react-transition-group/CSSTransition.js
+++ app/views/containers/trigger/react-transition-group/CSSTransition.js
@@ -1,14 +1,14 @@
-import PropTypes from 'prop-types';
-import addOneClass from 'dom-helpers/class/addClass';
+import PropTypes from 'prop-types'
+import addOneClass from 'dom-helpers/class/addClass'
 
-import removeOneClass from 'dom-helpers/class/removeClass';
-import React from 'react';
+import removeOneClass from 'dom-helpers/class/removeClass'
+import React from 'react'
 
-import Transition from './Transition';
-import { classNamesShape } from './utils/PropTypes';
+import Transition from './Transition'
+import { classNamesShape } from './utils/PropTypes'
 
-const addClass = (node, classes) => node && classes && classes.split(' ').forEach(c => addOneClass(node, c));
-const removeClass = (node, classes) => node && classes && classes.split(' ').forEach(c => removeOneClass(node, c));
+const addClass = (node, classes) => node && classes && classes.split(' ').forEach(c => addOneClass(node, c))
+const removeClass = (node, classes) => node && classes && classes.split(' ').forEach(c => removeOneClass(node, c))
 
 /**
  * A transition component inspired by the excellent
@@ -82,12 +82,12 @@ class CSSTransition extends React.Component {
   appliedClasses = {
     appear: {},
     enter: {},
-    exit: {},
+    exit: {}
   }
 
   onEnter = (node, appearing) => {
-    this.removeClasses(node, 'exit');
-    this.addClass(node, appearing ? 'appear' : 'enter', 'base');
+    this.removeClasses(node, 'exit')
+    this.addClass(node, appearing ? 'appear' : 'enter', 'base')
 
     if (this.props.onEnter) {
       this.props.onEnter(node, appearing)
@@ -95,7 +95,7 @@ class CSSTransition extends React.Component {
   }
 
   onEntering = (node, appearing) => {
-    const type = appearing ? 'appear' : 'enter';
+    const type = appearing ? 'appear' : 'enter'
     this.addClass(node, type, 'active')
 
     if (this.props.onEntering) {
@@ -105,8 +105,8 @@ class CSSTransition extends React.Component {
 
   onEntered = (node, appearing) => {
     const type = appearing ? 'appear' : 'enter'
-    this.removeClasses(node, type);
-    this.addClass(node, type, 'done');
+    this.removeClasses(node, type)
+    this.addClass(node, type, 'done')
 
     if (this.props.onEntered) {
       this.props.onEntered(node, appearing)
@@ -114,8 +114,8 @@ class CSSTransition extends React.Component {
   }
 
   onExit = (node) => {
-    this.removeClasses(node, 'appear');
-    this.removeClasses(node, 'enter');
+    this.removeClasses(node, 'appear')
+    this.removeClasses(node, 'enter')
     this.addClass(node, 'exit', 'base')
 
     if (this.props.onExit) {
@@ -132,8 +132,8 @@ class CSSTransition extends React.Component {
   }
 
   onExited = (node) => {
-    this.removeClasses(node, 'exit');
-    this.addClass(node, 'exit', 'done');
+    this.removeClasses(node, 'exit')
+    this.addClass(node, 'exit', 'done')
 
     if (this.props.onExited) {
       this.props.onExited(node)
@@ -141,11 +141,11 @@ class CSSTransition extends React.Component {
   }
 
   getClassNames = (type) => {
-    const { classNames } = this.props;
-    const isStringClassNames = typeof classNames === 'string';
+    const { classNames } = this.props
+    const isStringClassNames = typeof classNames === 'string'
     const prefix = isStringClassNames && classNames
       ? `${classNames}-`
-      : '';
+      : ''
 
     let baseClassName = isStringClassNames
       ? `${prefix}${type}`
@@ -153,59 +153,59 @@ class CSSTransition extends React.Component {
 
     let activeClassName = isStringClassNames
       ? `${baseClassName}-active`
-      : classNames[`${type}Active`];
+      : classNames[`${type}Active`]
 
     let doneClassName = isStringClassNames
       ? `${baseClassName}-done`
-      : classNames[`${type}Done`];
+      : classNames[`${type}Done`]
 
     return {
       baseClassName,
       activeClassName,
       doneClassName
-    };
+    }
   }
 
-  addClass(node, type, phase) {
-    let className = this.getClassNames(type)[`${phase}ClassName`];
+  addClass (node, type, phase) {
+    let className = this.getClassNames(type)[`${phase}ClassName`]
 
     if (type === 'appear' && phase === 'done') {
-      className += ` ${this.getClassNames('enter').doneClassName}`;
+      className += ` ${this.getClassNames('enter').doneClassName}`
     }
 
     // This is for to force a repaint,
     // which is necessary in order to transition styles when adding a class name.
     if (phase === 'active') {
       /* eslint-disable no-unused-expressions */
-      node && node.scrollTop;
+      node && node.scrollTop
     }
 
     this.appliedClasses[type][phase] = className
     addClass(node, className)
   }
 
-  removeClasses(node, type) {
+  removeClasses (node, type) {
     const {
       base: baseClassName,
       active: activeClassName,
       done: doneClassName
     } = this.appliedClasses[type]
 
-    this.appliedClasses[type] = {};
+    this.appliedClasses[type] = {}
 
     if (baseClassName) {
-      removeClass(node, baseClassName);
+      removeClass(node, baseClassName)
     }
     if (activeClassName) {
-      removeClass(node, activeClassName);
+      removeClass(node, activeClassName)
     }
     if (doneClassName) {
-      removeClass(node, doneClassName);
+      removeClass(node, doneClassName)
     }
   }
 
-  render() {
-    const { classNames: _, ...props } = this.props;
+  render () {
+    const { classNames: _, ...props } = this.props
 
     return (
       <Transition
@@ -217,7 +217,7 @@ class CSSTransition extends React.Component {
         onExiting={this.onExiting}
         onExited={this.onExited}
       />
-    );
+    )
   }
 }
 
@@ -329,7 +329,7 @@ CSSTransition.propTypes = {
    *
    * @type Function(node: HtmlElement)
    */
-  onExited: PropTypes.func,
-};
+  onExited: PropTypes.func
+}
 
 export default CSSTransition
--- app/views/index.js
+++ app/views/index.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import Container from './containers'
+// import Container from './containers'
+
+const useCountDown = (initCount = 0, endCallback) => {
+  const [count, setCount] = useState(initCount)
+  const [isStarted, setIsStarted] = useState(false)
+  const countRef = useRef()
+
+  useEffect(() => {
+    // use ref to store value
+    // see: https://zh-hans.reactjs.org/docs/hooks-faq.html#why-am-i-seeing-stale-props-or-state-inside-my-function
+    countRef.current = count
+  })
+
+  function doCountDown () {
+    const newCount = countRef.current ? countRef.current - 1 : countRef.current
+    setCount(newCount)
+    if (!isStarted) {
+      setIsStarted(true)
+    }
+    if (newCount === 0) {
+      if (endCallback) {
+        endCallback()
+      }
+    } else {
+      setTimeout(() => {
+        doCountDown()
+      }, 1000)
+    }
+  }
+
+  return [count, doCountDown, isStarted]
+}
+
+function App () {
+  const [count, startCount, isStarted] = useCountDown(20, () => { console.log('计数') })
+  return <div onClick={startCount}>
+    count: {isStarted ? count : '点击开始计数'}
+  </div>
+}
 
 ReactDOM.render(
-  <Container.Trigger />,
+  // <Container.Trigger />,
+  <App />,
   document.getElementById('root')
 )
